947b859116b3ce9311b8d00f911a0aaa2fd7f226
xVal4k <cristianbricicaru.cb@gmail.com>
2022-05-22 18:55:40 +0200

    fixed - create new user with used phone number

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'address' => ['required', 'string', 'max:255'],
-            'phone_num' => ['required', 'string', 'max:25'],
+            'phone_num' => ['required', 'string', 'max:25', 'unique:users'],
             'birth_date' => ['required', 'date']
         ]);
     }
